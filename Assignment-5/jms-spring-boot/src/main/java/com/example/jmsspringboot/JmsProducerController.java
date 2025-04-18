e776b07d3f4403157dde8c3da93d732b10d2e887
Mohan <n01620006@humber.ca>
2025-04-17 23:25:15 -0400

    Assignmnet-5

---
+++ Assignment-5/jms-spring-boot/src/main/java/com/example/jmsspringboot/JmsProducerController.java
@@ -0,0 +1,28 @@
+package com.example.jmsspringboot;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class JmsProducerController {
+
+    private final JmsTemplate jmsTemplate;
+
+    public JmsProducerController(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        jmsTemplate.convertAndSend("demo-queue", message);
+        return "Messagesent:" + message;
+    }
+
+    @PostMapping("/send")
+    public String sendPostMessage(@RequestBody String message) {
+        jmsTemplate.convertAndSend("lab-queue", message);
+        return "Message sent: " + message;
+    }
+
+}
